49c67b77f75ce9547a7b44da1520ead988b21c6e
Fysal Beauferris <fysalb@hotmail.com>
2024-10-26 15:55:03 -0600

    added .gitignore, updated vite.config to deal with compresion of assets for better optimization and updated default port # and added uponing on server start functionality

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import compress from 'vite-plugin-compression'; // enable compression to optimize assets
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), compress()],
+  server: {
+    port: 3000, // Update default port to 3000
+    open: true, // Open the browser on server start
+  },
 });
